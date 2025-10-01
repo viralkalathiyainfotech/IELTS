ad0873034b4f94e503aef553af1bffdc534f5d04
DarshanBackend <darshan1.kalathiyainfotech@gmail.com>
2025-10-01 09:50:04 +0530

    db connect string cghange

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,15 @@
 
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
 export const connectDB = async () =>{
     try {
-        await mongoose.connect(process.env.MONGO_DB_URL);
+        await mongoose.connect(process.env.MONGO_DB_URL || "mongodb+srv://darshan1kalathiyainfotech:IELTS%40123@ielts.ddd5mmh.mongodb.net/");
         console.log("DB Is Connected...");
     } catch (error) {
         console.error("DB Is Not Connected...",error);
         process.exit(1);
     }
 };
+
